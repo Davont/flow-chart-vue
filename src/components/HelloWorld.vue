a4870643c52dee73b01e103125c24de1b51be005
Davont <davontz@icloud.com>
2024-08-09 17:39:26 +0800

    feat: initialize Vue 3 project with Vite and basic setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,178 @@
+<script setup lang="tsx">
+import HuiCharts, { FlowChart } from "@opentiny/huicharts";
+import "@opentiny/huicharts/components/FlowChart/index.css";
+import Demo from './demo.vue'
+import { createApp,onMounted,h } from 'vue';
+import "./flow-chart.less";
+
+const option = {
+  // 节点纵向最小距离
+  vGap: 50,
+  // 节点横向最小距离
+  hGap: 80,
+  // 流程图连线形态
+  lineStyle: {
+    type: "Bezier",
+  },
+  // 绘制节点的回调，FlowChart会在计算流程图布局前调用
+  render: (container, data) => {
+    let id = data.id;
+    let nodeClass = "fc-node-example-red";
+    if (id.indexOf("3") !== -1) {
+      nodeClass = "fc-node-example-green";
+    }
+    if (id.indexOf("2") !== -1) {
+      nodeClass = "fc-node-example-orange";
+    }
+    if (id.indexOf("1") !== -1) {
+      nodeClass = "fc-node-example-blue";
+    }
+    // let node = `<div>22<div>`;
+    // let node = "<div className={`fc-node-example ${nodeClass}`}>{id}</div>";
+    // container.insertAdjacentHTML("beforeend", node);
+    return <Demo/>;
+  },
+  // 图表数据
+  data: {
+    nodes: [
+      {
+        id: "1",
+      },
+      {
+        id: "2",
+      },
+      {
+        id: "3",
+      },
+      {
+        id: "4",
+      },
+      {
+        id: "5",
+      },
+      {
+        id: "6",
+      },
+      {
+        id: "7",
+      },
+      {
+        id: "8",
+      },
+      {
+        id: "9",
+      },
+      {
+        id: "10",
+      },
+      {
+        id: "11",
+      },
+      {
+        id: "12",
+      },
+      {
+        id: "13",
+      },
+      {
+        id: "14",
+      },
+    ],
+    edges: [
+      {
+        source: "1",
+        target: "2",
+        lineStyle: {
+          dash: true,
+        },
+      },
+      {
+        source: "9",
+        target: "3",
+      },
+      {
+        source: "10",
+        target: "3",
+      },
+      {
+        source: "11",
+        target: "10",
+      },
+      {
+        source: "4",
+        target: "5",
+      },
+      {
+        source: "4",
+        target: "6",
+      },
+      {
+        source: "3",
+        target: "4",
+        lineStyle: {
+          dash: false,
+        },
+      },
+      {
+        source: "14",
+        target: "4",
+      },
+      {
+        source: "4",
+        target: "7",
+      },
+      {
+        source: "5",
+        target: "8",
+      },
+      {
+        source: "6",
+        target: "8",
+      },
+      {
+        source: "7",
+        target: "8",
+      },
+      {
+        source: "2",
+        target: "4",
+      },
+      {
+        source: "12",
+        target: "1",
+      },
+      {
+        source: "13",
+        target: "1",
+      },
+    ],
+  },
+};
+
+defineProps({
+  msg: String,
+})
+
+
+onMounted(() => {
+  let chartIns = new HuiCharts();
+    let chartContainerDom = document.getElementById("main");
+    console.log(chartContainerDom);
+    
+    chartIns.init(chartContainerDom);
+    let chartOption = option;
+    chartIns.setSimpleOption(FlowChart, chartOption);
+    chartIns.render();
+})
+
+</script>
+
+<template>
+  <div id="main" style="width: 600px;height:400px;"></div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/demo.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+defineProps({
+  msg: String,
+})
+
+
+
+</script>
+
+<template>
+  <div>
+
+    <h1>122334</h1>
+    <p>1111</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
